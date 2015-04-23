1c8f05058d09d1d098751b27e562a9087ef88415
Azahara <Azahara@Azahara-PC>
2015-04-23 12:04:07 +0200

    Concesionario de coches con ficheros. Version 1.0 (Final)

--- Concesionario/src/concesionarioConFicherosEmanuelGalvanFontalba/examenMarzo/concesionarioCoches/GestionFicheros.java
+++ Concesionario/src/concesionarioConFicherosEmanuelGalvanFontalba/examenMarzo/concesionarioCoches/GestionFicheros.java
@@ -8,11 +8,20 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.regex.Pattern;
 
 public class GestionFicheros {
+	
+	private Pattern patronExtension = Pattern.compile(".obj$");
 
 	static void guardar(Object object, String archivoName)throws IOException {
-		File archivo = new File(archivoName + ".obj");
+		File archivo ;
+		if(extensionValida(archivoName)){
+			archivo = new File(archivoName);
+		}
+		else{
+			archivo = new File(archivoName + ".obj");
+		}
 		try (ObjectOutputStream escrituraObjeto = new ObjectOutputStream(
 				new BufferedOutputStream(new FileOutputStream(archivo)));) {
 			escrituraObjeto.writeObject(object);
@@ -20,10 +29,18 @@ public class GestionFicheros {
 	}
 
 	static Object abrir(String archivoName) throws ClassNotFoundException, IOException {
-		File archivo = new File(archivoName + ".obj");
+		File archivo ;
+		if(extensionValida(archivoName))
+			archivo = new File(archivoName);
+		else
+			archivo = new File(archivoName + ".obj");
 		try (ObjectInputStream lecturaObjeto = new ObjectInputStream(
 				new BufferedInputStream(new FileInputStream(archivo)))) {
 			return lecturaObjeto.readObject();
 		}
+	}
+	
+	private static boolean extensionValida(String archivoName){
+		return archivoName.contains(".obj");
 	}
 }
